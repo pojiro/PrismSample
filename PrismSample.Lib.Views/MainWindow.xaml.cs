a0e9dc8b85dcdc5ac68f3cc5826f37a30a5ab581
pojiro <pojiro.jp@gmail.com>
2021-11-22 09:10:26 +0900

    Regionの表示

--- PrismSample.Lib.Views/MainWindow.xaml.cs
+++ PrismSample.Lib.Views/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System.Windows;
+using Prism.Ioc;
+using Prism.Regions;
+using Unity;
 
 namespace PrismSample.Lib.Views
 {
@@ -7,9 +10,21 @@ namespace PrismSample.Lib.Views
     /// </summary>
     public partial class MainWindow : Window
     {
+        [Dependency]
+        public IContainerExtension ContainerExtension { get; set; }
+
+        [Dependency]
+        public IRegionManager RegionManager { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            RegionManager.AddToRegion("OperandRegion", ContainerExtension.Resolve<OperandView>());
+            RegionManager.AddToRegion("AnswerRegion", ContainerExtension.Resolve<AnswerView>());
         }
     }
 }
